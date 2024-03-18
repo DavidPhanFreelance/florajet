d05bcc0b595613133f54013c1bc0a688cef0f089
sygreed <david.phan@outlook.fr>
2024-03-18 13:33:49 +0100

    add appendDatabse method

--- index test .php
+++ index test .php
@@ -23,8 +23,41 @@
     INSERT INTO article VALUES (4, 1, 'Article 4', 'Lorem ipsum dolor sit amet 4');
 */
 
-class ArticleAgregator
+class ArticleAgregator implements IteratorAggregate
 {
+    private $articles = [];
+
+    #[\ReturnTypeWillChange]
+    public function getIterator()
+    {
+        return new ArrayIterator($this->articles);
+    }
+
+    public function appendDatabase($host, $username, $password, $database)
+    {
+        $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+        $stmt = $pdo->query("SELECT * FROM article");
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $this->articles[] = (object) [
+                'name' => $row['name'],
+                'sourceName' => $row,
+                'content' => $row['content']
+            ];
+        }
+    }
+
+    public function appendRss($sourceName, $feedUrl)
+    {
+        $rss = simplexml_load_file($feedUrl);
+        foreach ($rss->channel->item as $item) {
+            $this->articles[] = (object) [
+                'name' => (string) $item->title,
+                'sourceName' => $sourceName,
+                'content' => (string) $item->description
+            ];
+        }
+    }
+
 }
 
 $a = new ArticleAgregator();
@@ -33,15 +66,17 @@ $a = new ArticleAgregator();
  * Récupère les articles de la base de données, avec leur source.
  * host, username, password, database name
  */
-$a->appendDatabase('localhost', 'root', '', 'florajet_test');
+$a->appendDatabase('localhost:3306', 'root', '', 'florajet_test');
 
 /**
  * Récupère les articles d'un flux rss donné
  * source name, feed url
  */
-$a->appendRss('Le Monde',    'http://www.lemonde.fr/rss/une.xml');
+
+//$a->appendRss('Le Monde',    'http://www.lemonde.fr/rss/une.xml');
 
 foreach ($a as $article) {
+//    var_dump($article);
     echo sprintf('<h2>%s</h2><em>%s</em><p>%s</p>',
         $article->name,
         $article->sourceName,
